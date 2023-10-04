b67df59a8cb97993ed9e990229fde301e1460410
noreyb <deneb@denebnoMacBook-Pro.local>
2023-10-04 23:39:11 +0900

    update: another container

--- containers.py
+++ containers.py
@@ -4,15 +4,17 @@ from dependency_injector import containers, providers
 from repository.interface.bookmark import IBookmarkHandler
 from repository.interface.storage import IStorageHandler
 from repository.pcloud import PCloudHandler
-from repository.pocket import PocketHandler
+from repository.nitter_pocket import NitterPocketHandler
+from repository.ngk_pocket import NGKPocketHandler
 from usecase.interface.image_downloader import IImageDownloader
 from usecase.nitter_image_downloader import NitterImageDownloader
+from usecase.ngk_image_downloader import NGKImageDownloader
 
 
 class NitterContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
     bookmark_handler = providers.Factory(
-        PocketHandler, token=config.token, consumer_key=config.consumer_key
+        NitterPocketHandler, token=config.token, consumer_key=config.consumer_key
     )
     storage_handler = providers.Factory(
         PCloudHandler,
@@ -27,6 +29,23 @@ class NitterContainer(containers.DeclarativeContainer):
         output_dir=config.output_dir,
     )
 
+class NGKContainer(containers.DeclarativeContainer):
+    config = providers.Configuration()
+    bookmark_handler = providers.Factory(
+        NGKPocketHandler, token=config.token, consumer_key=config.consumer_key
+    )
+    storage_handler = providers.Factory(
+        PCloudHandler,
+        username=config.pc_username,
+        password=config.pc_password,
+        save_dir=config.save_dir,
+    )
+    image_downloader = providers.Factory(
+        NGKImageDownloader,
+        bookmark_handler=bookmark_handler,
+        storage_handler=storage_handler,
+        output_dir=config.output_dir,
+    )
 
 # future work
 """
