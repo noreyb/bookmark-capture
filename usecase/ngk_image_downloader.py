95753272739d43d04403c6aaff921d347d2648dc
noreyb <deneb@denebnoMacBook-Pro.local>
2023-10-04 23:12:46 +0900

    add: for NGK routine

---
+++ usecase/ngk_image_downloader.py
@@ -0,0 +1,53 @@
+import time
+
+import requests
+from PIL import Image, UnidentifiedImageError
+
+from repository.interface.bookmark import IBookmarkHandler
+from repository.interface.storage import IStorageHandler
+from usecase.interface.image_downloader import IImageDownloader
+
+
+class NGKImageDownloader(IImageDownloader):
+    def __init__(
+        self,
+        bookmark_handler: IBookmarkHandler,
+        storage_handler: IStorageHandler,
+        output_dir: str,
+    ) -> None:
+        self.bookmark = bookmark_handler
+        self.storage = storage_handler
+        self.output_dir = output_dir
+
+    def run(self) -> None:
+        # pocketのurlを取得
+        urls = self.bookmark.get_unread_items()
+        # 画像ダウンロード
+        for url in urls:
+            try:
+                output_path = self.download_image(url)
+            except Exception as e:
+                print(e)
+                continue
+            self.storage.upload_file(output_path)
+            self.bookmark.archive_item(url)
+        return None
+
+    def download_image(self, url: str) -> str:
+        file_name = url.split("/")[-1]
+
+        # ファイル名の揺れを吸収するための処理
+        if "?" in file_name:
+            file_name = file_name.split("?")[0]
+        if len(file_name) >= 200:
+            file_name = file_name[-200:]
+        if "." not in file_name:
+            raise Exception(f"file_name: {file_name}, url: {url}")
+
+        with open(f"{output_dir}/{file_name}", "wb") as f:
+            f.write(r.content)
+        try:
+            image = Image.open(f"{output_dir}/{file_name}")
+        except UnidentifiedImageError:
+            raise Exception(f"UnidentifiedImageError: {file_name}, url: {url}")
+        image.save(f"{output_dir}/{file_name}", quality=85)
