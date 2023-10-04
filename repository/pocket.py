f081da6f6187d4e7cbe24b5b9b4d806e1fb0688c
noreyb <deneb@denebnoMacBook-Pro.local>
2023-10-04 22:34:02 +0900

    add: implement archive item

--- repository/pocket.py
+++ repository/pocket.py
@@ -1,5 +1,6 @@
 from repository.interface.bookmark import IBookmarkHandler
 import requests
+import time
 
 
 class PocketHandler(IBookmarkHandler):
@@ -25,4 +26,38 @@ class PocketHandler(IBookmarkHandler):
         return [item["given_url"] for item in resp["list"].values()]
 
     def archive_item(self, url) -> None:
-        return super().archive_item(url)
+        # get item_id from url
+        endpoint = "https://getpocket.com/v3/get"
+        resp = requests.post(
+            endpoint,
+            data={
+                "consumer_key": self.consumer_key,
+                "access_token": self.token,
+                "search": url,
+            },
+        )
+        # Error handling 
+        if resp.status_code != requests.codes.ok:
+            raise Exception(f"status_code: {resp.status_code}, url: {url}")
+        
+        resp = resp.json()
+        time.sleep(1)
+        item_id = resp["list"][list(resp["list"].keys())[0]]["item_id"]
+
+        # archive item
+        endpoint = "http://getpocket.com/v3/send"
+        data = {
+            "consumer_key": self.consumer_key,
+            "access_token": self.token,
+            "actions": [
+                {
+                    "action": "archive",
+                    "item_id": item_id,
+                }
+            ],
+        }
+        headers = {"Content-type": "application/json", "X-accept": "application/json"}
+        resp = requests.post(endpoint, json=data, headers=headers)
+        if resp.status_code != requests.codes.ok:
+            raise Exception(f"status_code: {resp.status_code}, url: {url}")
+        time.sleep(1)
