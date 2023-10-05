c5126ff21a782afa1db1393d7982fb46eea4f982
noreyb <deneb@denebnoMacBook-Pro.local>
2023-10-05 14:37:43 +0900

    update: create base class for common process

--- repository/ngk_pocket.py
+++ repository/ngk_pocket.py
@@ -2,14 +2,10 @@ import time
 
 import requests
 
-from repository.interface.bookmark import IBookmarkHandler
+from repository.base_pocket import BasePocketHandler
 
 
-class NGKPocketHandler(IBookmarkHandler):
-    def __init__(self, token: str, consumer_key: str) -> None:
-        self.token = token
-        self.consumer_key = consumer_key
-
+class NGKPocketHandler(BasePocketHandler):
     def get_unread_items(self) -> list:
         url = "http://getpocket.com/v3/get"
         data = {
@@ -26,40 +22,3 @@ class NGKPocketHandler(IBookmarkHandler):
             exit()
 
         return [item["given_url"] for item in resp["list"].values()]
-
-    def archive_item(self, url) -> None:
-        # get item_id from url
-        endpoint = "https://getpocket.com/v3/get"
-        resp = requests.post(
-            endpoint,
-            data={
-                "consumer_key": self.consumer_key,
-                "access_token": self.token,
-                "search": url,
-            },
-        )
-        # Error handling
-        if resp.status_code != requests.codes.ok:
-            raise Exception(f"status_code: {resp.status_code}, url: {url}")
-
-        resp = resp.json()
-        time.sleep(1)
-        item_id = resp["list"][list(resp["list"].keys())[0]]["item_id"]
-
-        # archive item
-        endpoint = "http://getpocket.com/v3/send"
-        data = {
-            "consumer_key": self.consumer_key,
-            "access_token": self.token,
-            "actions": [
-                {
-                    "action": "archive",
-                    "item_id": item_id,
-                }
-            ],
-        }
-        headers = {"Content-type": "application/json", "X-accept": "application/json"}
-        resp = requests.post(endpoint, json=data, headers=headers)
-        if resp.status_code != requests.codes.ok:
-            raise Exception(f"status_code: {resp.status_code}, url: {url}")
-        time.sleep(1)
