75140a22dbf750350f0b9a66c646e65cdf3f7c33
noreyb <137875776+noreyb@users.noreply.github.com>
2024-11-15 17:26:53 +0900

    Update fdv_image_downloader.py

--- usecase/fdv_image_downloader.py
+++ usecase/fdv_image_downloader.py
@@ -32,7 +32,7 @@ class FDVImageDownloader(IImageDownloader):
                 print(e)
                 continue
             self.storage.upload_file(output_path)
-            self.bookmark.archive_item(url)
+            self.bookmark.delete_item(url)
         return None
 
     def download_image(self, url: str) -> str:
