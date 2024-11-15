8012ded44e6d11ae7c6274cafbc8f15b7d16a70b
noreyb <137875776+noreyb@users.noreply.github.com>
2024-11-15 17:23:47 +0900

    Update base_pocket.py for delete action

--- repository/base_pocket.py
+++ repository/base_pocket.py
@@ -49,3 +49,44 @@ class BasePocketHandler(IBookmarkHandler):
         if resp.status_code != requests.codes.ok:
             raise Exception(f"status_code: {resp.status_code}, url: {url}")
         time.sleep(1)
+
+    
+    def delete_item(self, url) -> None:
+        # get item_id from url
+        endpoint = "https://getpocket.com/v3/get"
+        resp = requests.post(
+            endpoint,
+            data={
+                "consumer_key": self.consumer_key,
+                "access_token": self.token,
+                "search": url,
+            },
+        )
+        # Error handling
+        if resp.status_code != requests.codes.ok:
+            raise Exception(f"status_code: {resp.status_code}, url: {url}")
+
+        resp = resp.json()
+        time.sleep(1)
+        if resp["status"] == 2:
+            # This item has been deleted
+            return None
+        item_id = resp["list"][list(resp["list"].keys())[0]]["item_id"]
+
+        # archive item
+        endpoint = "http://getpocket.com/v3/send"
+        data = {
+            "consumer_key": self.consumer_key,
+            "access_token": self.token,
+            "actions": [
+                {
+                    "action": "delete",
+                    "item_id": item_id,
+                }
+            ],
+        }
+        headers = {"Content-type": "application/json", "X-accept": "application/json"}
+        resp = requests.post(endpoint, json=data, headers=headers)
+        if resp.status_code != requests.codes.ok:
+            raise Exception(f"status_code: {resp.status_code}, url: {url}")
+        time.sleep(1)
